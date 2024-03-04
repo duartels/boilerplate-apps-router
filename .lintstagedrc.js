aaf3612e5ad160578277f37826b493c9c01082eb
Luan Duarte <luansouza789@gmail.com>
2024-03-04 14:32:56 -0300

    initial tests added

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `yarn run lint --fix . ${filenames.join(' --file')}`
+    `yarn lint --fix . ${filenames.join(' --file')}`,
+    `yarn test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
