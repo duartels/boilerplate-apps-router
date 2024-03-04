0d969f763b5240a876f1a256cb2e78cd6cf76517
Luan Duarte <luansouza789@gmail.com>
2024-03-04 14:48:23 -0300

    remove unused files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `yarn lint --fix . ${filenames.join(' --file')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`,
     `yarn test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
